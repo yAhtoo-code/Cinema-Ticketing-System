b6c017223197d938f42860c03644ac18c25da568
johnhnts <hintayjohnpatrick@gmail.com>
2025-10-19 16:00:28 +0800

    ok

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-// Movies page (root)
 Route::get('/', function () {
     return view('movies'); 
 })->name('movies');
 
-// Homepage
 Route::get('/homepage', [HomeController::class, 'index'])->name('homepage');
+
 
 
 
